4eea4e3587d2ffe81a77d371115b58ec82d7b57e
Leana Neuber <leas@Leanas-MacBook-Pro-2.local>
2021-09-21 15:55:51 +0200

    Change to shiny dashboard

--- app.R
+++ app.R
@@ -1,9 +1,10 @@
 addResourcePath(prefix = 'www', directoryPath = './www')
+
+library(shinydashboard)
 library(DT)
-library(bslib)
 library(ggplot2)
 
-fr <- list(
+de <- list(
   sProcessing = "Verarbeitung läuft...",
   sSearch = "Suche&nbsp;:",
   sLengthMenu = "Zeige _MENU_ Einträge",
@@ -22,143 +23,6 @@ fr <- list(
   ),
   oAria = list(sSortAscending = ": aktivieren, um die Spalte in aufsteigender Reihenfolge zu sortieren",
                sSortDescending = ": activer aktivieren, um die Spalte in absteigender Reihenfolge zu sortieren")
-)
-
-
-datentab <-
-  fluidPage(theme = bslib::bs_theme(bootswatch = "flatly"),
-            sidebarLayout(
-              sidebarPanel(
-                h2("Willkommen!"),
-                width = 3,
-                p(
-                  "Hier seht ihr unser aktuelles Datenset, was sich täglich aktualisiert. Stay tuned!"
-                ),
-                # Button
-                downloadButton("downloadData", "Ich will die Daten!"),
-                br(),
-                br(),
-                img(
-                  src = "www/wir.png",
-                  height = 100,
-                  style = "display: block; margin-left: auto; margin-right: auto;"
-                ),
-                br()
-              ),
-              mainPanel(dataTableOutput("table"), width = 9)
-            ))
-
-
-infotab <- fluidPage(
-  tags$head(tags$script(
-    HTML(
-      'var fakeClick = function(tabName) {
-       var dropdownList = document.getElementsByTagName("a");
-       for (var i = 0; i < dropdownList.length; i++) {
-       var link = dropdownList[i];
-       if(link.getAttribute("data-value") == tabName) {
-       link.click();
-       };
-       }
-       };'
-    )
-  )),
-  # WHAT
-  fluidRow(column(3),
-           column(
-             6,
-             shiny::HTML(
-               "<br><br><center> <h1>Was Ihr hier finden werdet</h1> </center><br>"
-             ),
-             shiny::HTML(
-               "<h5>Eine Möglichkeit Daten zu verstehen und zu visualisieren.... TODO</h5>"
-             )
-           ),
-           column(3)),
-  
-  fluidRow(style = "height:50px;"),
-  
-  # PAGE BREAK
-  tags$hr(),
-  
-  # HOW
-  fluidRow(column(3),
-           column(
-             6,
-             shiny::HTML(
-               "<br><br><center> <h1>Wie diese Seite Euch helfen kann</h1> </center><br>"
-             ),
-             shiny::HTML(
-               "<h5>Ihr könnt die Daten anschauen und frei entscheiden, was Ihr wie visualisieren wollt. Außerdem helfen wir Euch bei den Übungen und erklären Euch
-               interessante Konzepte hier und auf unserem Instagram Account oder auf Moodle:</h5>"
-             )
-           ),
-           column(3)),
-  fluidRow(style = "height:25px;"),
-  fluidRow(column(3),
-           column(
-             6,
-             tags$div(
-               align = "center",
-               tags$a("Instagram",
-                      href = "https://www.instagram.com/everydata_ude/",
-                      class = "btn btn-primary btn-lg"),
-               tags$a("Moodle",
-                      href = "https://moodle.uni-due.de/course/view.php?id=29686",
-                      class = "btn btn-primary btn-lg"),
-             )
-           ),
-           column(3)),
-  fluidRow(style = "height:50px;"),
-  
-  # PAGE BREAK
-  tags$hr(),
-  
-  # WHERE
-  fluidRow(column(3),
-           column(
-             6,
-             shiny::HTML("<br><br><center> <h1>Wer wir sind</h1> </center><br>"),
-             shiny::HTML(
-               "<h5>Wir sind Ella, Lea und Katrin und arbeiten an
-               der UDE in Essen im Arbeitsbereich von Frau Heine.</h5>"
-             )
-           ),
-           column(3)),
-  
-  fluidRow(column(
-    12,
-    align = "center",
-    div(style = "display: inline-block;", img(src = "www/wir_baum.png", width =
-                                                "50%"))
-  )),
-  
-  
-  # PAGE BREAK
-  tags$hr(),
-  
-  
-  fluidRow(column(3),
-           column(
-             6,
-             shiny::HTML("<br><br><center> <h1>Bereit loszulegen?</h1> </center><br>"),
-             shiny::HTML(
-               "<h5>Wir empfehlen Euch, mit der Betrachtung der Visualisierungen zu beginnen.</h5>"
-             )
-           ),
-           column(3)),
-  fluidRow(style = "height:25px;"),
-  fluidRow(column(3),
-           column(6,
-                  tags$div(
-                    align = "center",
-                    tags$a("Los geht's!",
-                           onclick = "fakeClick('vis')",
-                           class = "btn btn-primary btn-lg")
-                  )),
-           column(3)),
-  fluidRow(style = "height:25px;")
-  
 )
 
 kontakttab <- fluidPage(
@@ -249,70 +113,293 @@ kontakttab <- fluidPage(
   fluidRow(style = "height:150px;")
 )
 
-
-vistab <- fluidPage(
-  titlePanel(title = h4("Visualisierungen", align = "center")),
-  sidebarLayout(sidebarPanel(
-    radioButtons(
-      "category",
-      "Wähle eine Spalte:",
-      choices = c("Name", "Kategorie", "Kosten", "Zeit", "Geschmack", "Fun")
-    ),
-    conditionalPanel(
-      "input.category == 'Zeit' | input.category == 'Kosten'| input.category == 'Geschmack'| input.category == 'Fun'",
-      sliderInput(
-        inputId = "bins",
-        label = "Number of bins:",
-        min = 1,
-        max = 10,
-        value = 3
-      )
-    ),
-    width = 3
-  ),
-  mainPanel(plotOutput("hist"), width = 9)
-)
-)
-
-
-ui <- navbarPage(
-  theme = bslib::bs_theme(bootswatch = "flatly"),
-  "EveryData",
-  selected = "vis",
-  tabPanel("Überblick", infotab),
-  tabPanel("Daten", datentab),
-  tabPanel("Visualisierungen", vistab, value = "vis"),
-  tabPanel(
-    "Übungen",
-    "Sobald es mit den Übungen losgeht, findet Ihr hier Tips und Tricks... :)"
-  ),
-  tabPanel("Kontakt", kontakttab)
-)
-
-
-# Define server logic
-server <- function(input, output) {
-  datatable <- read.csv("init_food.csv", sep = ";")
-  customcol <- c("#FFDB6D", "#C4961A", "#F4EDCA", 
-                  "#D16103", "#C3D7A4", "#52854C", "#4E84C4", "#293352")
-  output$hist <- renderPlot({
-    category <- toString(input$category)
-    if (category == "Zeit" | category == "Kosten"| category == "Geschmack"| category == "Fun")  {ggplot(data = datatable, aes_string(x = category)) + geom_histogram( bins=input$bins, fill = "#098474")} 
-    else{ggplot(data = datatable, aes_string(x = category)) + geom_bar(stat="count", fill = "#098474") } 
-    
-    # qplot(datatable$category,
-    #       geom="histogram",
-    #       main = paste("Histogram for ", category), 
-    #       xlab = category,  g
-    #       fill=I("blue"), 
-    #       col=I("red"), 
-    #       alpha=I(.2))
-  })
+infotab <- fluidPage(
+  tags$head(tags$script(
+    HTML(
+      'var fakeClick = function(tabName) {
+       var dropdownList = document.getElementsByTagName("a");
+       for (var i = 0; i < dropdownList.length; i++) {
+       var link = dropdownList[i];
+       if(link.getAttribute("data-value") == tabName) {
+       link.click();
+       };
+       }
+       };'
+    )
+  )),
+  # WHAT
+  fluidRow(column(3),
+           column(
+             6,
+             shiny::HTML(
+               "<br><br><center> <h1>Was Ihr hier finden werdet</h1> </center><br>"
+             ),
+             shiny::HTML(
+               "<h5>Eine Möglichkeit Daten zu verstehen und zu visualisieren.... TODO</h5>"
+             )
+           ),
+           column(3)),
   
+  fluidRow(style = "height:50px;"),
+  
+  # PAGE BREAK
+  tags$hr(),
+  
+  # HOW
+  fluidRow(column(3),
+           column(
+             6,
+             shiny::HTML(
+               "<br><br><center> <h1>Wie diese Seite Euch helfen kann</h1> </center><br>"
+             ),
+             shiny::HTML(
+               "<h5>Ihr könnt die Daten anschauen und frei entscheiden, was Ihr wie visualisieren wollt. Außerdem helfen wir Euch bei den Übungen und erklären Euch
+               interessante Konzepte hier und auf unserem Instagram Account oder auf Moodle:</h5>"
+             )
+           ),
+           column(3)),
+  fluidRow(style = "height:25px;"),
+  fluidRow(column(3),
+           column(
+             6,
+             tags$div(
+               align = "center",
+               tags$a("Instagram",
+                      href = "https://www.instagram.com/everydata_ude/",
+                      class = "btn btn-primary btn-lg"),
+               tags$a("Moodle",
+                      href = "https://moodle.uni-due.de/course/view.php?id=29686",
+                      class = "btn btn-primary btn-lg"),
+             )
+           ),
+           column(3)),
+  fluidRow(style = "height:50px;"),
+  
+  # PAGE BREAK
+  tags$hr(),
+  
+  # WHERE
+  fluidRow(column(3),
+           column(
+             6,
+             shiny::HTML("<br><br><center> <h1>Wer wir sind</h1> </center><br>"),
+             shiny::HTML(
+               "<h5>Wir sind Ella, Lea und Katrin und arbeiten an
+               der UDE in Essen im Arbeitsbereich von Frau Heine.</h5>"
+             )
+           ),
+           column(3)),
+  fluidRow(style = "height:50px;"),
+  fluidRow(
+    # column(3),
+    
+    # Ella
+    column(4,
+           div(
+             # class = "panel panel-default",
+             div(
+               class = "panel-body",
+               width = "600px",
+               align = "center",
+               div(tags$img(
+                 src = "www/ella.png",
+                 width = "50px",
+                 height = "50px"
+               )),
+               div(tags$h5("Ella"),
+                   tags$h6(
+                     tags$i("Psychologin, Projektleiterin und Instagram-Beauftragte")
+                   )),
+               div(style = "width:10%;"),
+               div("Ich liebe es, blablabla....")
+             )
+           )),
+    # Katrin
+    column(4,
+           div(
+             # class = "panel panel-default",
+             div(
+               class = "panel-body",
+               width = "600px",
+               align = "center",
+               div(tags$img(
+                 src = "katrin.png",
+                 width = "50px",
+                 height = "50px"
+               )),
+               div(tags$h5("Katrin"),
+                   tags$h6(
+                     tags$i("Mensch-Computer Mensch und kreativer Kopf")
+                   )),
+               div(style = "height:10%;"),
+               div("Grafiken? Kann ich. Blablab")
+             )
+           )),
+    # Lea
+    column(4,
+           div(
+             # class = "panel panel-default",
+             div(
+               class = "panel-body",
+               width = "600px",
+               align = "center",
+               div(tags$img(
+                 src = "lea.png",
+                 width = "50px",
+                 height = "50px"
+               )),
+               div(tags$h5("Lea"),
+                   tags$h6(
+                     tags$i("Informatikerin und Verantwortlich für die Shiny-App")
+                   )),
+               div(style = "height:10%;"),
+               div(
+                 "Wenn es um die Entwicklung von IT-Lösungen in interdisziplinären Teams geht bin ich immer gern dabei!"
+               )
+             )
+           )),
+    # column(3)
+    
+  ),
+  
+  
+  # PAGE BREAK
+  tags$hr(),
+  
+  
+  fluidRow(column(3),
+           column(
+             6,
+             shiny::HTML("<br><br><center> <h1>Bereit loszulegen?</h1> </center><br>"),
+             shiny::HTML(
+               "<h5>Wir empfehlen Euch, mit der Betrachtung der Visualisierungen zu beginnen.</h5>"
+             )
+           ),
+           column(3)),
+  fluidRow(style = "height:25px;"),
+  fluidRow(column(3),
+           column(6,
+                  tags$div(
+                    align = "center",
+                    tags$a("Los geht's!",
+                           onclick = "fakeClick('vis')",
+                           class = "btn btn-primary btn-lg")
+                  )),
+           column(3)),
+  fluidRow(style = "height:25px;")
+  
+)
+
+datentab <-
+  fluidPage(
+            sidebarLayout(
+              sidebarPanel(
+                h2("Willkommen!"),
+                width = 4,
+                p(
+                  "Hier seht ihr unser aktuelles Datenset, was sich täglich aktualisiert. Stay tuned!"
+                ),
+                # Button
+                downloadButton("downloadData", "Ich will die Daten!"),
+              ),
+              mainPanel(dataTableOutput("table"), width = 8)
+            ))
+
+
+ui <- dashboardPage(
+  skin = "yellow",
+  dashboardHeader(title = "EveryData",
+                  dropdownMenu(
+                    type = "notifications", 
+                    headerText = strong("Kontakt"), 
+                    icon = icon("address-card"),
+                    badgeStatus = NULL,
+                    notificationItem(
+                      text = ("Ella: ella.posny@uni-due.de"),
+                      icon = icon("female")
+                    ),
+                    notificationItem(
+                      text = ("Katrin: katrin.hartmann@uni-due.de"),
+                      icon = icon("female")
+                    ),
+                    notificationItem(
+                      text = ("Lea: leana.neuber@uni-due.de"),
+                      icon = icon("female")
+                    ),
+                    notificationItem(
+                      text = ("Frau Angela Heine: angela.heine@uni-due.de"),
+                      icon = icon("chalkboard-teacher")
+                    )
+                    )),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("chart-bar")),
+      menuItem("Daten", tabName = "data", icon = icon("table")),
+      menuItem("Übungen", tabName = "exercises", icon = icon("award")),
+      menuItem("Info", tabName = "info", icon = icon("question"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                infoBoxOutput("Essen"),
+                infoBoxOutput("Zeit"),
+                infoBoxOutput("Ausgaben")
+              ),
+              
+              fluidRow(
+                box(title="Histogram",plotOutput("plot1", height = 250)),
+                
+                box(
+                  title = "Kategorie",
+                  radioButtons(
+                    "category",
+                    "Wähle eine Spalte:",
+                    choices = c("Name", "Kategorie", "Kosten", "Zeit", "Geschmack", "Fun")
+                  ),
+                  conditionalPanel(
+                    "input.category == 'Zeit' | input.category == 'Kosten'| input.category == 'Geschmack'| input.category == 'Fun'",
+                    sliderInput(
+                      inputId = "bins",
+                      label = "Number of bins:",
+                      min = 1,
+                      max = 10,
+                      value = 3
+                    )
+                  )
+                )
+              ),
+              fluidRow(
+                box(title="Wer isst am meisten?", plotOutput("pie1"))
+              )
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "data",
+              datentab
+      ),
+      
+      tabItem(tabName = "exercises",
+              "Sobald es mit den Übungen losgeht, findet Ihr hier Tips und Tricks... :)"
+      ),
+      tabItem("info", infotab)
+    )
+  )
+)
+
+server <- function(input, output) {
+
+  datatable <- read.csv("init_food.csv", sep = ";")
+
+  output$Essen <- renderInfoBox({infoBox("Essen", nrow(datatable), icon = icon("hamburger"), color = "purple", fill=TRUE)})
+  output$Zeit <- renderInfoBox({infoBox("Minuten", sum(datatable$Zeit), icon = icon("clock"), fill = TRUE)})
+  output$Ausgaben <- renderInfoBox({infoBox("Euro", sum(datatable$Kosten), icon = icon("euro-sign"),color="yellow", fill = TRUE)})
   output$table <- DT::renderDataTable({
     datatable
-  },  rownames = FALSE, options = list(language = fr))
-  
+  },  rownames = FALSE, options = list(language = de))
+
   # Downloadable csv of selected dataset
   output$downloadData <- downloadHandler(
     filename = function() {
@@ -323,7 +410,19 @@ server <- function(input, output) {
     }
   )
   
+  output$plot1 <-  renderPlot({
+    if (input$category == "Zeit" | input$category == "Kosten"| input$category == "Geschmack"| input$category == "Fun")  {ggplot(data = datatable, aes_string(x = input$category)) + geom_histogram( bins=input$bins, fill = "#098474")} 
+    else{ggplot(data = datatable, aes_string(x = input$category)) + geom_bar(stat="count", fill = "#098474") } 
+  })
+  output$pie1 <- renderPlot({
+    ggplot(datatable, aes(x=factor(1), fill=datatable[,"Name"])) +
+      geom_bar(stat="count") +
+      # scale_y_continuous(breaks = seq(0,12,3), labels = c("0", "25%", "50%", "75%", "100%")) + 
+      coord_polar("y", start=0)+
+      theme_void()+ 
+      theme(legend.position="right") +
+      guides(fill=guide_legend(title="Name"))
+  })
 }
 
-# Run the app ----
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
